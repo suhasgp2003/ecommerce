25b504aa25151d34d6be49489befdcf9c4228b76
Suhas G P <suhas234567@gmail.com>
2026-07-14 19:53:29 +0530

    allowed host

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(ssvk&x)y-@djo43wt4e^6f$%oegy0qyptb68hu3d^+m$_nkbx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://ecommerce-hida.onrender.com']
 
 
 # Application definition

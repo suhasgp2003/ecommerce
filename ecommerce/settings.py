b0576325f31a4887e4c0b1b1e216a8117650a858
Suhas G P <suhas234567@gmail.com>
2026-07-14 20:02:33 +0530

    solved the issue in allwed host

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(ssvk&x)y-@djo43wt4e^6f$%oegy0qyptb68hu3d^+m$_nkbx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://ecommerce-hida.onrender.com']
+ALLOWED_HOSTS = ['ecommerce-hida.onrender.com']
 
 
 # Application definition
